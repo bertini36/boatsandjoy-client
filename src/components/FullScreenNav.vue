a092058bbe143593859935509ad55b7d84b57bda
Alberto Pou <albertopouquiros@gmail.com>
2021-02-10 08:45:26 +0100

    Hover svgs

--- src/components/FullScreenNav.vue
+++ src/components/FullScreenNav.vue
@@ -8,7 +8,9 @@
             <img src="../assets/img/favicon.png" alt="main logo" class="cursor-pointer h-12"/>
           </div>
           <button aria-label="close" @click.prevent="hideFullScreenNav" class="text-xl text-gray-500 mt-8 mr-8">
-            <img src="../assets/icons/icon-close-big.svg" alt="close icon">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 fill-current text-black hover:text-orange-600 cursor-pointer">
+             <path d="M19.571.429c-.571-.572-1.428-.572-2 0L10 8 2.429.429c-.572-.572-1.429-.572-2 0-.572.571-.572 1.428 0 2L8 10 .429 17.571c-.572.572-.572 1.429 0 2 .285.286.571.429 1 .429.428 0 .714-.143 1-.429L10 12l7.571 7.571c.286.286.715.429 1 .429.286 0 .715-.143 1-.429.572-.571.572-1.428 0-2L12 10l7.571-7.571c.572-.572.572-1.429 0-2z" transform="translate(-134 -194) translate(140 200)"/>
+           </svg>
           </button>
         </div>
         <ul class="mb-auto mt-12">
