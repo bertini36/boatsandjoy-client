66a73a88dfafbe74df42837cba6ffd71730f761b
Alberto Pou <albertopouquiros@gmail.com>
2021-02-21 17:10:56 +0100

    Add models to checkout form

--- src/components/CheckoutForm.vue
+++ src/components/CheckoutForm.vue
@@ -1,10 +1,10 @@
 <template>
   <form class="flex flex-col">
     <div class="inline-flex mb-3">
-      <span :class="{'price-circle': !applyResidentDiscount, 'disabled-price-circle': applyResidentDiscount}">
+      <span :class="{'price-circle': !isResident, 'disabled-price-circle': isResident}">
         {{ price }}€
       </span>
-      <span class="price-circle ml-2" v-if="applyResidentDiscount">
+      <span class="price-circle ml-2" v-if="isResident">
         {{ residentPrice }}€
       </span>
       <h1 class="title-left pt-3 ml-4">{{ availabilityOption.boat.name }}</h1>
@@ -15,25 +15,25 @@
     </p>
 
     <label class="inline-flex items-center cursor-pointer">
-      <input type="checkbox" class="h-5 w-5 cursor-pointer" v-model="applyResidentDiscount">
+      <input type="checkbox" class="h-5 w-5 cursor-pointer" v-model="isResident">
       <span class="pl-3 text-xs md:text-sm">{{ i18n.$t('checkout_resident') }}</span>
     </label>
 
     <label class="inline-flex items-center mt-1 cursor-pointer">
-      <input type="checkbox" class="h-5 w-5 cursor-pointer">
+      <input type="checkbox" class="h-5 w-5 cursor-pointer" v-model="acceptLegalAdvice">
       <span class="pl-3 text-xs md:text-sm">{{ i18n.$t('checkout_legal_advice') }}</span>
     </label>
 
     <label class="inline-flex items-center mt-1 cursor-pointer">
-      <input type="checkbox" class="h-5 w-5 cursor-pointer">
+      <input type="checkbox" class="h-5 w-5 cursor-pointer" v-model="acceptTermsAndConditions">
       <span class="pl-3 text-xs md:text-sm">{{ i18n.$t('checkout_terms_and_conditions') }}</span>
     </label>
 
-    <input type="text" class="custom-input mt-4" :placeholder="i18n.$t('checkout_name')">
+    <input type="text" class="custom-input mt-4" :placeholder="i18n.$t('checkout_name')" v-model="name" required>
 
-    <input type="number" class="custom-input mt-2" :placeholder="i18n.$t('checkout_telephone')">
+    <input type="number" class="custom-input mt-2" :placeholder="i18n.$t('checkout_telephone')" v-model="telephone" required>
 
-    <textarea rows="2" class="custom-textarea mt-2" :placeholder="i18n.$t('checkout_extras')"></textarea>
+    <textarea rows="2" class="custom-textarea mt-2" :placeholder="i18n.$t('checkout_extras')" v-model="extras"></textarea>
 
     <div class="flex">
       <button class="btn mt-6 w-full flex-grow">
@@ -61,13 +61,23 @@ export default {
     const price = ref(basePrice);
     const residentPrice = ref(basePrice - (basePrice * residentDiscount));
 
-    const applyResidentDiscount = ref(false);
+    const isResident = ref(false);
+    const acceptLegalAdvice = ref(false);
+    const acceptTermsAndConditions = ref(false);
+    const name = ref('');
+    const telephone = ref('');
+    const extras = ref('');
 
     return {
       i18n,
-      applyResidentDiscount,
       price,
       residentPrice,
+      isResident,
+      acceptLegalAdvice,
+      acceptTermsAndConditions,
+      name,
+      telephone,
+      extras
     }
   }
 }
