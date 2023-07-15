973c7a044275b67b16721a2d3143612032746920
Alberto Pou <albertopouquiros@gmail.com>
2023-07-15 15:26:04 +0200

    test4

--- src/components/BoatsSection.vue
+++ src/components/BoatsSection.vue
@@ -1,16 +1,36 @@
 <template>
   <div id="boats">
-    HOLAAAA
+    <article class="py-12 md:pt-20 flex flex-row justify-center">
+      <div class="flex flex-col w-full px-4 lg:px-0 lg:w-3/5">
+        <h3 class="title mb-4">{{ $t('boats_title') }}</h3>
+      </div>
+    </article>
+
+    <article class="py-2 md:py-8 lg:py-10">
+      <div class="grid grid-rows-1 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-4 lg:gap-8 xl:gap-14 lg:px-8 xl:px-20 2xl:px-60">
+        <div v-for="image_url in image_urls" :key="image_url">
+          <img :src="image_url" alt="Boat photo" class="rounded-sm md:cursor-pointer" @click="showModal(image_url)">
+        </div>
+      </div>
+    </article>
+
+    <modal :showing="showingModal" @close="showingModal = false">
+      <div class="flex flex-col w-full">
+        <img :src="selectedImageUrl" alt="Boat photo" class="rounded-sm">
+      </div>
+    </modal>
+
   </div>
 </template>
 
 <script>
 import { ref } from 'vue';
 
+import Modal from '../components/Modal.vue';
 import { isBigScreen } from '../utils/screen';
 
 export default {
-  components: { },
+  components: { Modal },
 
   setup() {
     const image_urls = [
