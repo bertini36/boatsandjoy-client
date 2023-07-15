4a5ff9292def52438ef461507aa9f27eec0394e8
Alberto Pou <albertopouquiros@gmail.com>
2023-07-15 15:28:06 +0200

    fix

--- src/components/BoatsSection.vue
+++ src/components/BoatsSection.vue
@@ -14,6 +14,59 @@
       </div>
     </article>
 
+    <article class="px-4 lg:px-8 xl:px-20 2xl:px-60 my-16">
+      <div class="w-full flex-none -ml-full rounded-3xl bg-gray-100">
+        <div class="w-full flex-none rounded-3xl transform shadow-lg bg-gradient-to-r from-green-300 to-blue-500 -rotate-2 sm:-rotate-2">
+          <div class="grid grid-flow-col grid-rows-1 grid-cols-3">
+            <div class="hidden lg:block row-start-1 col-start-1 col-span-1 pl-4 pt-4">
+              <div class="transform scale-90 -rotate-2">
+                <img class="rounded-2xl shadow-2xl cursor-pointer" src="../assets/img/boats4.jpg" alt="Boat photo"
+                     @click="showModal(require('../assets/img/boats4.jpg'))">
+              </div>
+            </div>
+
+            <div class="bg-white xl:relative row-start-1 col-start-1 lg:col-start-2 col-span-3 lg:col-span-2 transform translate-x-4 lg:translate-x-8 translate-y-4 lg:translate-y-7 pt-8 px-8 md:px-8 lg:px-12 rounded-3xl text-left bg-white shadow-2xl">
+              <h2 class="title text-2xl font-bold mb-6">{{ $t('boats_information_title') }}</h2>
+              <div class="grid grid-flow-col grid-rows-2 grid-cols-2 md:grid-rows-1 md:grid-cols-3 gap-4 pt-4">
+                <div>
+                  <strong>{{ $t('boats_basic') }}</strong>
+                  <ul class="mt-2">
+                    <li>{{ $t('boats_max_capacity') }}</li>
+                    <li>{{ $t('boats_power') }}</li>
+                    <li>{{ $t('boats_location') }}</li>
+                    <li>{{ $t('boats_caution') }}</li>
+                    <li>{{ $t('boats_gas') }}</li>
+                  </ul>
+                </div>
+                <div class="pt-4 md:pt-0">
+                  <strong>{{ $t('boats_equiped_with') }}</strong>
+                  <ul class="mt-2">
+                    <li>{{ $t('boats_solarium') }}</li>
+                    <li>{{ $t('boats_bimini') }}</li>
+                    <li>{{ $t('boats_fridge') }}</li>
+                    <li>{{ $t('boats_bag') }}</li>
+                  </ul>
+                </div>
+                <div>
+                  <strong>{{ $t('boats_extras') }}</strong>
+                  <ul class="mt-2">
+                    <li>{{ $t('boats_masks') }}</li>
+                    <li>{{ $t('boats_catering') }}</li>
+                    <li>{{ $t('boats_drinks') }}</li>
+                    <li>{{ $t('boats_alcohol') }}</li>
+                    <li>{{ $t('boats_watersports') }}</li>
+                  </ul>
+                </div>
+              </div>
+              <h3 class="mt-4 md:mt-6 text-lg font-semibold mb-6 text-orange-500 text-center">
+                {{ $t('boats_ask') }}
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+
     <modal :showing="showingModal" @close="showingModal = false">
       <div class="flex flex-col w-full">
         <img :src="selectedImageUrl" alt="Boat photo" class="rounded-sm">
